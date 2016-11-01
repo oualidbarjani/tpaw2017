71f4478f53b9677fb3b80473045768b7b0625721
tp3oualidbarjani <walid.barjani@gmail.com>
2016-11-01 03:31:42 +0100

    Update formulaire.js

--- tp3/formulaire.js
+++ tp3/formulaire.js
@@ -10,14 +10,15 @@ $( document ).ready(function() {
 	    e.preventDefault();
 	    var mymodal = $('#myModal');
 
-	    if(nom.value == '' || prenom.value == '' || dateNaissance.value == '' || adresse.value == '' || adresse.value == '' ){
+	    if(nom.value == '' || prenom.value == '' || dateNaissance.value == '' || adresse.value == '' || mail.value == '' ){
 	    	mymodal.find('.modal-body').text('Veuillez saisir tout les champs !');
 	   		$(".modal").modal('show')
 
 	   	} else {
-	   		mymodal.find('.modal-title').text('Bonjour ' + nom.value + " " + prenom.value);
-	   		mymodal.find('.modal-body').text('Vous êtes nés le : ' + dateNaissance.value + " et vous habitez : ");
-	   		$(".modal").modal('show')
+	   		mymodal.find('.modal-title').text('Bonjour ' + " " + prenom.value);
+	   		mymodal.find('.modal-body').text('Vous êtes nés le : ' + dateNaissance.value + " et vous habitez : ");      
+	   		 mymodal.find('.modal-maps').html(" <img src='https://maps.googleapis.com/maps/api/staticmap?center="+adresse.value+"&zoom=16&size=400x200&path=weight:3%7Ccolor:blue%7Cenc:{coaHnetiVjM??_SkM??~R&key=AIzaSyCmQS716Lawj3T_dgBVlj_eXZ33-CrcdCY'/></br></br>"+adresse.value);
+			$(".modal").modal('show')
 
 	   	}
   });
