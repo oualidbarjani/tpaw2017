1fbcd8bfc463eb5f105f02f1d1bf8a573df6ce78
tp3oualidbarjani <walid.barjani@gmail.com>
2016-11-09 19:41:13 +0100

    Update meteo.js

--- tp5/meteo.js
+++ tp5/meteo.js
@@ -5,7 +5,47 @@ window.onload=function(){
 		console.log(ville);
 		rechercheville(ville);
 	});
+	document.getElementById("gps").addEventListener("click",function(event){
+	event.preventDefault();
+	var ville=document.getElementById("ville").value;
+	function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
 }
+
+function showPosition(position) {
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
+    +latlon+"&zoom=14&size=400x300&sensor=false";
+	
+    document.getElementById("gps").innerHTML = "<img src='"+img_url+"'>";
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
+	recherchelatlng(ville);
+	});
+	
+	
+	
 function rechercheville(_ville){
 	
 	console.log(rechercheville,'Hello from '+_ville);
@@ -17,14 +57,14 @@ function rechercheville(_ville){
 			var resp = JSON.parse(request.responseText);
 			var nom= resp.name;
 			var ico = resp.weather[0].icon;
-			var temperature = (resp.main.temp - 273.15).toFixed(0) + "°";
+			var temperature = (resp.main.temp - 273.15).toFixed(0) ;
 			var prec = resp.rain;
 			var hum = resp.main.humidity;
 			var nuage = resp.clouds.all;
 			var vent = resp.wind.speed;
 			
 			document.getElementById('result1').innerHTML = nom ;
-			document.getElementById('result2').innerHTML = temperature ;
+			document.getElementById('result2').innerHTML = temperature +"°";
 			document.getElementById('result3').innerHTML = resp.weather[0].description ;
 			document.getElementById('result4').innerHTML = " Humidity : "+hum+"%" ;
 			document.getElementById('result5').innerHTML = "Nuage : "+nuage+"%" ;
@@ -47,4 +87,4 @@ function rechercheville(_ville){
 function recherchelatlng(_ville){
 	console.log(rechercheville,'Hello from'+_lat+','+_lng);
 	
-}
+}}
