bcaaf5cdbd7ad422e8d716f4e3137e5fa9618d36
tp3oualidbarjani <walid.barjani@gmail.com>
2016-11-10 14:50:54 +0100

    Update formulaire.js

--- tp4/formulaire.js
+++ tp4/formulaire.js
@@ -40,7 +40,40 @@ $( document ).ready(function() {
       //Afficher un texte de légende en fonction du nombre de caractères
         $("#compteur4").html(""+ rellength + " car ");
     });
+	document.getElementById("gps").addEventListener("click",function(event){
+		event.preventDefault();
+		
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(showPosition, showError);
+		} else {
+			gps1.innerHTML = "Geolocation is not supported by this browser.";
+		}
+	});
+function showPosition(ville) {
+    var latlon = ville.coords.latitude + "," + ville.coords.longitude;
+
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
+    +latlon+"&zoom=14&size=400x300&sensor=false";
 	
+    document.getElementById("gps").innerHTML = "<img src='"+img_url+"'>";
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            gps1.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            gps1.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            gps1.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            gps1.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
 		$('#valider').click(function(e){
 	    e.preventDefault();
 	    var mymodal = $('#myModal');
